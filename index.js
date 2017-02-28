a203d7c7a76d25fe60d91996eb3ba4d41487be9e
blghns <saglikb@wit.edu>
2017-02-28 17:35:28 -0500

    Linted code, fixed projection scaling

--- index.js
+++ index.js
@@ -2,24 +2,21 @@ var svg = d3.select("body").append("svg").attr("width", "100%").attr("height", "
 var svgGroup = svg.append("g");
 var width = parseInt(svg.style('width'));
 var height = parseInt(svg.style('height'));
-var projection = d3.geoMercator().scale(300).center([0, 0]).translate([width / 2, height / 2]);
+var projection = d3.geoMercator().scale(3400).center([2.5, 46.5]).translate([width / 2, height / 2]);
 var path = d3.geoPath().projection(projection);
 
-d3.json("departements.geojson", function (error, departements){
+d3.json("departements.geojson", function (error, departements) {
 
+    svgGroup.append("g")
+            .attr("class", "departements")
+            .selectAll("path")
+            .data(departements.features)
+            .enter().append("path")
+            .attr("d", path);
 
-
-	svgGroup.append("g")
-      .attr("class", "departements")
-    .selectAll("path")
-    .data(departements.features)
-    .enter().append("path")
-      .attr("d", path);	 
-
-
-  svg.call(d3.zoom()
-        .scaleExtent([1/2, 500])
-        .on("zoom", zoomed));
+    svg.call(d3.zoom()
+               .scaleExtent([1 / 2, 500])
+               .on("zoom", zoomed));
 });
 
 function zoomed() {
